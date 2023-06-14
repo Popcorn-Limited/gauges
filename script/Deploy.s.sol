2df70b205edc21b7c1dfbee14a5f871d1db07bc0
ZeframLou <zeframlou@gmail.com>
2023-06-13 17:13:30 -0700

    feat: add deploy scripts for crosschain factories

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: AGPL-3.0
 pragma solidity ^0.8.13;
 
 import {IVaultRegistry, VaultMetadata} from "popcorn/src/interfaces/vault/IVaultRegistry.sol";
--- script/DeployGauges.s.sol
+++ script/DeployGauges.s.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: AGPL-3.0
 pragma solidity ^0.8.13;
 
 import {CREATE3Script, console} from "./base/CREATE3Script.sol";
