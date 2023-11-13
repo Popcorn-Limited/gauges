348bbc65d4cb4e917b2bd03d7995f28e25247d67
RedVeil <leon.niesler@gmail.com>
2023-11-13 13:46:20 +0100

    maxSupply check for minting vcx

--- src/VCX.sol
+++ src/VCX.sol
@@ -5,22 +5,29 @@ import {ERC20} from "solmate/tokens/ERC20.sol";
 import {Owned} from "solmate/auth/Owned.sol";
 
 contract VCX is ERC20, Owned {
-    IERC20 internal constant POP = IERC20(0xD0Cd466b34A24fcB2f87676278AF2005Ca8A78c4);
+    IERC20 internal constant POP =
+        IERC20(0xD0Cd466b34A24fcB2f87676278AF2005Ca8A78c4);
     uint public endOfMigrationTs;
+    uint256 maxSupply = 1e26; // @dev cant mint more than 100m VCX
 
     event Migrated(address indexed user, uint popAmount, uint vcxAmount);
     event UpdatedEndOfMigrationTs(uint oldTs, uint newTs);
 
-    constructor(string memory _name, string memory _symbol) ERC20(_name, _symbol, 18) Owned(msg.sender) {
+    constructor(
+        string memory _name,
+        string memory _symbol
+    ) ERC20(_name, _symbol, 18) Owned(msg.sender) {
         endOfMigrationTs = block.timestamp + 365 days;
     }
 
     // @param amount the amount of POP you want to migrate
     function migrate(address to, uint amount) external {
         if (block.timestamp > endOfMigrationTs) revert("CLOSED");
+        uint256 toMint = amount * 10;
+        if (totalSupply + toMint > maxSupply) revert("MAX_SUPPLY");
 
         POP.transferFrom(msg.sender, address(this), amount);
-        _mint(to, amount * 10);
+        _mint(to, toMint);
 
         emit Migrated(msg.sender, amount, amount * 10);
     }
